91e90505d4909cab69a1e90b3f9c65c1da47b707
sebastian-kremer <s.kremer@student.uw.edu.pl>
2021-05-15 14:13:45 +0200

    ...

--- R_Script_DataAnalysis.R
+++ R_Script_DataAnalysis.R
@@ -5,13 +5,13 @@ summary(data$age)
 
 data <- within(data, {   
   Age.group <- NA # need to initialize variable
-  Age.group[age < 20] <- "under 20"
+  Age.group[age < 20] <- "0-20"
   Age.group[age >=20 & age <30] <- "20-29"
   Age.group[age >=30 & age <40] <- "30-39"
   Age.group[age >=40 & age <50] <- "40-49"
   Age.group[age >=50 & age <60] <- "50-59"
   Age.group[age >=60 & age <70] <- "60-69"
-  Age.group[age >=70 & age <30] <- "over 70"
+  Age.group[age >=70 ] <- "over 70"
 
 
 } )
