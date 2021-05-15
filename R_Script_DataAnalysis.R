e8251a6752ff4550446374c52876ebc03f855664
Lukasz Marchel <l.marchel@student.uw.edu.pl>
2021-05-15 14:39:30 +0200

    model with full data

--- R_Script_DataAnalysis.R
+++ R_Script_DataAnalysis.R
@@ -1,4 +1,9 @@
 
+library(dplyr)
+library(readr)
+library(caret)
+library(lmtest)
+
 data <- read.csv("dataset_31_credit-g.csv")
 
 summary(data$age)
@@ -24,3 +29,29 @@ data$Gender[data$personal_status == "'female div/dep/mar'"] <- "female"
 data$Gender[data$personal_status == "'male single'"] <- "male"
 data$Gender[data$personal_status == "'male div/sep'"] <- "male"
 data$Gender[data$personal_status == "'male mar/wid'"] <- "male"
+
+
+data$Class[data$class == "good"] <- 1
+data$Class[data$class == "bad"] <- 0
+
+# Logistic Regression
+
+set.seed(987654321)
+
+data_full_training <- createDataPartition(data$Class,
+                                            p = 0.7, 
+                                            list = FALSE) 
+data_full_train <- data[data_full_training,]
+data_full_test <- data[-data_full_training,]
+
+
+data_logit1 <- glm(Class ~ checking_status + duration + credit_history + purpose +
+                     credit_amount + savings_status + employment + installment_commitment + 
+                     other_parties + residence_since + property_magnitude + age + other_payment_plans +
+                     housing + existing_credits + job + num_dependents + own_telephone + foreign_worker +
+                     Gender,
+                    family =  binomial(link = "logit"),
+                    data = data_full_train)
+
+summary(data_logit1)
+
